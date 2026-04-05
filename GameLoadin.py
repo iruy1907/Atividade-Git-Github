b7a1c49592e46ffec9f2703111bd7952e64398f3
Yuri <yuriale1907@gmail.com>
2026-04-05 19:14:58 -0300

    correção e finalização do código

--- GameLoadin.py
+++ GameLoadin.py
@@ -11,7 +11,10 @@ while loading <= load:
     print(f"\r[{barra}] {progresso}%   ", end="")
 
     time.sleep(0.5)
-    loading += random.randint(1,10) 
+    loading += random.randint(1,6) 
+
+barra = chr(9608) * load
+print(f"\r[{barra}] 100%{' ' * 10}")
 
 print("\n Loading Concluido")
 print("\033[?25h")
