1dc57287b29f4ace1c3a5f6ff3dc9210b3e5ed47
Yuri <yuriale1907@gmail.com>
2026-04-05 18:31:17 -0300

    adição da barra de loading

--- GameLoadin.py
+++ GameLoadin.py
@@ -2,3 +2,9 @@ import time, random
 
 load = 100
 loading = 0
+
+while loading <= load:
+    progresso = int((loading/load)*100)
+    barra = chr(9608) * loading +" " * (load - loading)
+
+    print(f"\r[{barra}] {progresso}%    ", end="")
